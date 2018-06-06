7540be84273b392c19a3fb3e8c95a6947b119f5c
ryoutoku <invisible060@gmail.com>
2018-06-07 08:36:50 +0900

    feat: サンプル実行追加

--- src/sample_main.py
+++ src/sample_main.py
@@ -11,7 +11,14 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def main():
+def plot(Individuals):
+    x = [x.gene[0] for x in Individuals]
+    y = [x.gene[1] for x in Individuals]
+    plt.scatter(x, y, s=5)
+    plt.show()
+
+
+def demo_1():
     # 遺伝子長
     dimension = 30
 
@@ -23,7 +30,6 @@ def main():
 
     # 評価関数
     evaluator = Sphere()
-    evaluator = Rosenbrock()
 
     # 親の選択と交叉
     # 親の数は交叉方法に合わせて設定する
@@ -49,12 +55,43 @@ def main():
     plot(society.individuals)
 
 
-def plot(Individuals):
-    x = [x.gene[0] for x in Individuals]
-    y = [x.gene[1] for x in Individuals]
-    plt.scatter(x, y, s=5)
-    plt.show()
+def demo_2():
+    # 遺伝子長
+    dimension = 30
+
+    # 集団数
+    individual_num = 300
+
+    # 世代数の最大
+    generation_loop = 1000
+
+    # 評価関数
+    evaluator = Sphere()
+
+    # 親の選択と交叉
+    # 親の数は交叉方法に合わせて設定する
+    roulette = RouletteSelector(2)
+    blx = Simplex(dimension * 10)
+
+    # 次世代に残す個体の選択
+    society = Society(
+        evaluator,
+        blx,
+        roulette,
+        JGG())
+    generator = Generator(10., -10., dimension)
+
+    society.generate_individuals(individual_num, generator)
+    plot(society.individuals)
+
+    for i in range(generation_loop):
+        society.change_generation()
+        ind = society.get_best_individual()
+        print(i, ind.evaluate_value)
+
+    plot(society.individuals)
 
 
 if __name__ == "__main__":
-    main()
+    # demo_1()
+    demo_2()
