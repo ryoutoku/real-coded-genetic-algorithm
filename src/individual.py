4d0c72527e497e57e18c56d43e8c217a388ca86f
ryoutoku <invisible060@gmail.com>
2018-06-10 08:44:11 +0900

    fix: 変数名修正

--- src/individual.py
+++ src/individual.py
@@ -8,7 +8,7 @@ class Individual(object):
     _evaluator = None
 
     def __init__(self, gene):
-        self._genome = np.array(gene)
+        self._gene = np.array(gene)
         self._evaluate_value = Individual._evaluator.evaluate(self)
 
     @classmethod
@@ -19,7 +19,7 @@ class Individual(object):
     def gene(self):
         """遺伝子
         """
-        return self._genome
+        return self._gene
 
     @property
     def evaluate_value(self):
